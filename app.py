04a0107e2da6015dfb3edc1908048e9ff4c4f6f5
Junko Noami <64884506+noamister@users.noreply.github.com>
2020-06-23 21:32:32 +0900

    ガチャ１回回した機能をPython上で実装。結果はコンソールに出力される。
    ヘッダー画像とファビコンも変更

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 # splite3をimportする
 import sqlite3
+# randomを使えるようにする
+import random
 # flaskをimportしてflaskを使えるようにする
 from flask import Flask , render_template , request , redirect , session
 # appにFlaskを定義して使えるようにしています。Flask クラスのインスタンスを作って、 app という変数に代入しています。
@@ -17,11 +19,13 @@ def index():
 
     return render_template('index.html')
 
-# ガチャ後ページ
+# ガチャ実行後ページ
 @app.route('/gacha/<int:money>',methods=["GET", "POST"])
 def gacha(money):
-    money = money #ボタンから取ってくる値 100 or 200 or 300
-    budget = money #残金 最初はmoneyと同じ 
+        # 選んだ金額
+    money = money # ボタンから取ってくる値 100 or 200 or 300
+        # 残っている金額
+    budget = money # 残金 最初はmoneyと同じ 
 
 #    while budget > 0:
 
@@ -30,254 +34,49 @@ def gacha(money):
     c = conn.cursor()
     c.execute("select * from items where price <= ?",(budget,))
     conn.commit()
-        # fetchoneはタプル型
+        # fetchoneはタプル型 fetchall
     candidate = c.fetchall()
     conn.close()
-        # candidateの中身を確認 ターミナルに出力されるよ。
-    print("candidate の中身")    
-    print(candidate)
 
-    menu = candidate
+    # if len(candidate) == 0:
+    #     break
     
-    print("menu の中身")    
+        # Pythonでリストからランダムに要素を選択するchoice,   
+        # https://note.nkmk.me/python-random-choice-sample-choices/
+    food = random.choice(candidate)
+        # 残ったお金は
+    budget = budget - int(food[1])
+        # 空のmenuリストを用意
+    menu = []
+    menu.append(food)
+
+    print("---")
+    print("money の中身 ガチャの予算")    
+    print(money)
+    print("candidate の中身 ガチャで選ばれる可能性があるのはこのなかのどれか！")    
+    print(candidate)
+    print("---")
+    print("menu の中身 ガチャで選ばれたお菓子はこれ！")    
     print(menu)
-        # [('うまい棒', 10, 'img001'), ('ブラックサンダー', 30, 'img002'), ('ココアシガレット', 30, 'img003')]
-        # こんなのがcandidateの中身
-    return render_template('gacha.html', money = money, menu = menu)
+    print("残金は")
+    print(budget)
+    print("---")
+
+
+#    while budget > 0 ループを抜けた後で
+
+    # お釣り計算
+    budget = money - budget
+
+    return render_template('gacha.html', money = money, menu = menu, budget = budget)
 
 
 
-        #   user_id = session["user_id"]
-        # conn = sqlite3.connect("flaskapp.db")
-        # c = conn.cursor()
-
-        # # ゴグインしているuser nameを取得してuser_nameに代入
-        # c.execute("select name from user where id = ?", (user_id,))
-        # user_name = c.fetchone()[0]
-
-        # c.execute("select id, task from task where user_id = ?", (user_id,)) # task全てDBから取得
-        # task_list = [] #このtaskリストは最初は空だが、for文で埋まっていく
-        # for row in c.fetchall():  #fetchaiiは多次元配列（リストの中のリスト）でくる。わかりにくい。
-        #     # インデックス管理わかりにくい => idやtaskというkey,キーバリューで管理するためにデータの整形
-        #     task_list.append({"id":row[0], "task":row[1]}) #appendでその後ろの（）内のデータを追加する。
-        # c.close()
-        # print(task_list)
-        # return render_template("task_list.html", user_name = user_name, task_list = task_list) #forループで追加したtask_listを、htmlに渡す。
-        # # return "タスクが取れたかターミナルで確認"
- 
-
-# お買い物計算ページ
+# お買い物計算ページ まだ中身はない。
 @app.route('/calc',methods=["GET", "POST"])
 def calc():
     return render_template('calc.html')
 
-
-# @app.route('/register',methods=["GET", "POST"])
-# def register():
-#     #  登録ページを表示させる
-#     if request.method == "GET":
-#         if 'user_id' in session :
-#             return redirect ('/bbs')
-#         else:
-#             return render_template("register.html")
-
-#     # ここからPOSTの処理
-#     else:
-#         # 登録ページで登録ボタンを押した時に走る処理
-#         name = request.form.get("name")
-#         password = request.form.get("password")
-
-#         conn = sqlite3.connect('service.db')
-#         c = conn.cursor()
-#         # 課題4の答えはここ
-#         c.execute("insert into user values(null,?,?,'no_img.png')", (name,password))
-#         conn.commit()
-#         conn.close()
-#         return redirect('/login')
-
-
-# # GET  /login => ログイン画面を表示
-# # POST /login => ログイン処理をする
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     if request.method == "GET":
-#         if 'user_id' in session :
-#             return redirect("/bbs")
-#         else:
-#             return render_template("login.html")
-#     else:
-#         # ブラウザから送られてきたデータを受け取る
-#         name = request.form.get("name")
-#         password = request.form.get("password")
-
-#         # ブラウザから送られてきた name ,password を userテーブルに一致するレコードが
-#         # 存在するかを判定する。レコードが存在するとuser_idに整数が代入、存在しなければ nullが入る
-#         conn = sqlite3.connect('service.db')
-#         c = conn.cursor()
-#         c.execute("select id from user where name = ? and password = ?", (name, password) )
-#         user_id = c.fetchone()
-#         conn.close()
-#         # DBから取得してきたuser_id、ここの時点ではタプル型
-#         print(type(user_id))
-#         # user_id が NULL(PythonではNone)じゃなければログイン成功
-#         if user_id is None:
-#             # ログイン失敗すると、ログイン画面に戻す
-#             return render_template("login.html")
-#         else:
-#             session['user_id'] = user_id[0]
-#             return redirect("/bbs")
-
-
-# @app.route("/logout")
-# def logout():
-#     session.pop('user_id',None)
-#     # ログアウト後はログインページにリダイレクトさせる
-#     return redirect("/login")
-
-
-# @app.route('/bbs')
-# def bbs():
-#     if 'user_id' in session :
-#         user_id = session['user_id']
-#         conn = sqlite3.connect('service.db')
-#         c = conn.cursor()
-#         # # DBにアクセスしてログインしているユーザ名と投稿内容を取得する
-#         # クッキーから取得したuser_idを使用してuserテーブルのnameを取得
-#         c.execute("select name,prof_img from user where id = ?", (user_id,))
-#         # fetchoneはタプル型
-#         user_info = c.fetchone()
-#         # user_infoの中身を確認
-
-#         # 課題1の答えはここ del_flagが0のものだけ表示する
-#         # 課題2の答えはここ 保存されているtimeも表示する
-#         c.execute("select id,comment,time from bbs where userid = ? and del_flag = 0 order by id", (user_id,))
-#         comment_list = []
-#         for row in c.fetchall():
-#             comment_list.append({"id": row[0], "comment": row[1], "time":row[2]})
-
-#         c.close()
-#         return render_template('bbs.html' , user_info = user_info , comment_list = comment_list)
-#     else:
-#         return redirect("/login")
-
-
-
-# @app.route('/add', methods=["POST"])
-# def add():
-#     user_id = session['user_id']
-
-#     # 課題2の答えはここ 現在時刻を取得
-#     time = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-
-#     # POSTアクセスならDBに登録する
-#     # フォームから入力されたアイテム名の取得(Python2ならrequest.form.getを使う)
-#     comment = request.form.get("comment")
-#     conn = sqlite3.connect('service.db')
-#     c = conn.cursor()
-#     # 現在の最大ID取得(fetchoneの戻り値はタプル)
-
-#     # 課題1の答えはここ null,?,?,0の0はdel_flagのデフォルト値
-#     # 課題2の答えはここ timeを新たにinsert
-#     c.execute("insert into bbs values(null,?,?,0,?)", (user_id, comment,time))
-#     conn.commit()
-#     conn.close()
-#     return redirect('/bbs')
-
-# @app.route('/edit/<int:id>')
-# def edit(id):
-#     if 'user_id' in session :
-#         conn = sqlite3.connect('service.db')
-#         c = conn.cursor()
-#         c.execute("select comment from bbs where id = ?", (id,) )
-#         comment = c.fetchone()
-#         conn.close()
-
-#         if comment is not None:
-#             # None に対しては インデクス指定できないので None 判定した後にインデックスを指定
-#             comment = comment[0] # "りんご" ○   ("りんご",) ☓
-#             # fetchone()で取り出したtupleに 0 を指定することで テキストだけをとりだす
-#         else:
-#             return "アイテムがありません" # 指定したIDの name がなければときの対処
-
-#         item = { "id":id, "comment":comment }
-
-#         return render_template("edit.html", comment=item)
-#     else:
-#         return redirect("/login")
-
-
-# # /add ではPOSTを使ったので /edit ではあえてGETを使う
-# @app.route("/edit")
-# def update_item():
-#     if 'user_id' in session :
-#         # ブラウザから送られてきたデータを取得
-#         item_id = request.args.get("item_id") # id
-#         print(item_id)
-#         item_id = int(item_id) # ブラウザから送られてきたのは文字列なので整数に変換する
-#         comment = request.args.get("comment") # 編集されたテキストを取得する
-
-#         # 既にあるデータベースのデータを送られてきたデータに更新
-#         conn = sqlite3.connect('service.db')
-#         c = conn.cursor()
-#         c.execute("update bbs set comment = ? where id = ?",(comment,item_id))
-#         conn.commit()
-#         conn.close()
-
-#         # アイテム一覧へリダイレクトさせる
-#         return redirect("/bbs")
-#     else:
-#         return redirect("/login")
-
-# @app.route('/del' , methods=["POST"])
-# def del_task():
-#     id = request.form.get("comment_id")
-#     id = int(id)
-#     conn = sqlite3.connect('service.db')
-#     c = conn.cursor()
-#     # 指定されたitem_idを元にDBデータを削除せずにdel_flagを1にして一覧からは表示しないようにする
-#     # 課題1の答えはここ del_flagを1にupdateする
-#     c.execute("update bbs set del_flag = 1 where id=?", (id,))
-#     conn.commit()
-#     conn.close()
-#     # 処理終了後に一覧画面に戻す
-#     return redirect("/bbs")
-
-# #課題4の答えはここ
-# @app.route('/upload', methods=["POST"])
-# def do_upload():
-#     # bbs.tplのinputタグ name="upload" をgetしてくる
-#     upload = request.files['upload']
-#     # uploadで取得したファイル名をlower()で全部小文字にして、ファイルの最後尾の拡張子が'.png', '.jpg', '.jpeg'ではない場合、returnさせる。
-#     if not upload.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-#         return 'png,jpg,jpeg形式のファイルを選択してください'
-    
-#     # 下の def get_save_path()関数を使用して "./static/img/" パスを戻り値として取得する。
-#     save_path = get_save_path()
-#     # パスが取得できているか確認
-#     print(save_path)
-#     # ファイルネームをfilename変数に代入
-#     filename = upload.filename
-#     # 画像ファイルを./static/imgフォルダに保存。 os.path.join()は、パスとファイル名をつないで返してくれます。
-#     upload.save(os.path.join(save_path,filename))
-#     # ファイル名が取れることを確認、あとで使うよ
-#     print(filename)
-    
-#     # アップロードしたユーザのIDを取得
-#     user_id = session['user_id']
-#     conn = sqlite3.connect('service.db')
-#     c = conn.cursor()
-#     # update文
-#     # 上記の filename 変数ここで使うよ
-#     c.execute("update user set prof_img = ? where id=?", (filename,user_id))
-#     conn.commit()
-#     conn.close()
-
-#     return redirect ('/bbs')
-
-# #課題4の答えはここも
-# def get_save_path():
-#     path_dir = "./static/img"
-#     return path_dir
 
 
 @app.errorhandler(403)
